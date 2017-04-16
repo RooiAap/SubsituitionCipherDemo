88fbd803ebd1989f02dbcca35676e2f5a156757d
Brendan Oelofse <Oelofse30@gmail.com>
2017-04-16 10:48:36 +0200

    Added a change to the userinterface

--- decrypt.py
+++ decrypt.py
@@ -1,7 +1,7 @@
 alph = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']       #Normal order of alphabet
 p_alph = ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'z', 'x', 'c', 'v', 'b', 'n', 'm']     #Permutation order of alphabet
 
-c_text = input("Enter text to be decrypted: ")
+c_text = input("Enter ciphertext to be decrypted: ")
 p_text = []
 
 for i in range(len(c_text)):
